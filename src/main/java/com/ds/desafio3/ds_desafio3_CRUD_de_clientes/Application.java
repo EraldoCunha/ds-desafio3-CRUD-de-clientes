52ecbdcb2e4a0c0f7631d2b022e2f102009b3faa
Eraldo Cunha <eraldo_cunha@hotmail.com>
2024-09-02 10:58:17 -0300

    First commit

---
+++ src/main/java/com/ds/desafio3/ds_desafio3_CRUD_de_clientes/Application.java
@@ -0,0 +1,13 @@
+package com.ds.desafio3.ds_desafio3_CRUD_de_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds/desafio3/ds_desafio3_CRUD_de_clientes/DsDesafio3CrudDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.desafio3.ds_desafio3_CRUD_de_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsDesafio3CrudDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
