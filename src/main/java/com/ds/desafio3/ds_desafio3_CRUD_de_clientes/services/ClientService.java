dec240e911bd59a2913705416a532b1751769360
Eraldo Cunha <eraldo_cunha@hotmail.com>
2024-09-03 15:11:24 -0300

    PUT Client

--- src/main/java/com/ds/desafio3/ds_desafio3_CRUD_de_clientes/services/ClientService.java
+++ src/main/java/com/ds/desafio3/ds_desafio3_CRUD_de_clientes/services/ClientService.java
@@ -4,6 +4,7 @@ import com.ds.desafio3.ds_desafio3_CRUD_de_clientes.dto.ClientDTO;
 import com.ds.desafio3.ds_desafio3_CRUD_de_clientes.entities.Client;
 import com.ds.desafio3.ds_desafio3_CRUD_de_clientes.repositories.ClientRepository;
 import com.ds.desafio3.ds_desafio3_CRUD_de_clientes.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,20 +24,31 @@ public class ClientService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ClientDTO> findAll(Pageable pageable){
+    public Page<ClientDTO> findAll(Pageable pageable) {
         Page<Client> result = repository.findAll(pageable);
         return result.map(x -> new ClientDTO(x));
     }
 
     @Transactional
-    public ClientDTO insert(ClientDTO dto){
+    public ClientDTO insert(ClientDTO dto) {
         Client entity = new Client();
         copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
         return new ClientDTO(entity);
     }
 
-
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        try {
+            Client entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new ClientDTO(entity);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+    }
 
 
     private void copyDtoToEntity(ClientDTO dto, Client entity) {
